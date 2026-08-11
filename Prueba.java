c1527ba689641cb5710f711977402f068aada3e0
victormtz06 <victor.martinez25@itca.edu.sv>
2026-08-11 09:49:24 -0600

    Historial limpio sin JDK

---
+++ Prueba.java
@@ -0,0 +1,9 @@
+public class Prueba {
+    public static void main(String[] args) {
+        System.out.println("¡Conexión WSL - VS Code - Git funcionando correctamente!");
+
+        int a = 5;
+        int b = 3;
+        System.out.println("Suma de prueba: " + a + " + " + b + " = " + (a + b));
+    }
+}
